a53411eb6f3a5e42862b6ad647eb6d3046e18a5b
JCThePants <jcthepants@gmail.com>
2015-08-09 04:16:49 -0700

    add AbstractExtensionCommand

---
+++ src/com/jcwhatever/pvs/api/commands/AbstractExtensionCommand.java
@@ -0,0 +1,62 @@
+/*
+ * This file is part of PV-StarAPI for Bukkit, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) JCThePants (www.jcwhatever.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.jcwhatever.pvs.api.commands;
+
+import com.jcwhatever.nucleus.managed.commands.mixins.IVisibleCommand;
+import com.jcwhatever.nucleus.utils.PreCon;
+import com.jcwhatever.pvs.api.arena.IArena;
+import com.jcwhatever.pvs.api.arena.extensions.ArenaExtension;
+import org.bukkit.command.CommandSender;
+
+/**
+ * A base command intended for use with Arena extensions which includes
+ * PV-Star common utility methods.
+ */
+public abstract class AbstractExtensionCommand
+        extends AbstractPVCommand implements IVisibleCommand {
+
+    private final Class<? extends ArenaExtension> _extensionClass;
+
+    /**
+     * Constructor.
+     *
+     * <p>The command is expected to have an empty constructor. The empty constructor
+     * should invoke this constructor using the required arguments.</p>
+     *
+     * @param extensionClass  The class of the arena extension the command is for.
+     */
+    public AbstractExtensionCommand(Class<? extends ArenaExtension> extensionClass) {
+        PreCon.notNull(extensionClass);
+
+        _extensionClass = extensionClass;
+    }
+
+    @Override
+    public boolean isVisible(CommandSender sender) {
+
+        IArena arena = getSelectedArena(sender, ArenaReturned.ALWAYS);
+        return arena != null && arena.getExtensions().has(_extensionClass);
+    }
+}
